7cc2215d5efafe8a89ab8370127d3d1744e71b97
Sosan Neikbeen <sosan.neikbeen@gmail.com>
2021-10-14 23:10:11 +0200

    code refactor

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
-import history from "../history";
-// import { GoogleAuthProvider } from "../context/AuthContext";
 import PrivateRoute from "./routes/PrivateRoute";
 import { PostProvider } from "../context/PostContext";
 import { CommentProvider } from "../context/CommentContext";
+import { AuthProvider } from "../context/AuthContext";
+import { ToastProvider } from "react-toast-notifications";
 import PostCreate from "./posts/PostCreate";
 import PostList from "./posts/PostList";
 import PostDetail from "./posts/PostDetail";
@@ -14,16 +14,14 @@ import PostEdit from "./posts/PostEdit";
 import Profile from "./Profile";
 import ProfileEdit from "./ProfileEdit";
 import CommentDelete from "./comments/CommentDelete";
+import CommentEdit from "./comments/CommentEdit";
 import Signup from "./authentication/Register";
 import Login from "./authentication/Login";
-import { AuthProvider } from "../context/AuthContext";
-import { ToastProvider } from "react-toast-notifications";
-import CommentEdit from "./comments/CommentEdit";
 
 const App = () => {
   return (
     <div className="container">
-      <Router history={history}>
+      <Router>
         <div>
           <ToastProvider>
             <AuthProvider>
