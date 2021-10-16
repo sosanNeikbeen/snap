2a3f90efa22031635186de26697d2f4f430ac2e2
Sosan Neikbeen <sosan.neikbeen@gmail.com>
2021-10-16 09:13:52 +0200

    added .env file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFtHqVfOzvgAiM9sZYVizaqxeWuKf3Bp8",
-  authDomain: "social-media-app-4a0d8.firebaseapp.com",
-  projectId: "social-media-app-4a0d8",
-  storageBucket: "social-media-app-4a0d8.appspot.com",
-  messagingSenderId: "529010135007",
-  appId: "1:529010135007:web:127527cc62d3fd60957e6a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
